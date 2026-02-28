b8cbfb3f8f7f8af137c5c40e56bde74736cec385
NaveenPantra <naveenpantra.np@gmail.com>
2026-02-28 15:05:02 +0530

    home page brackets

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,52 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import "./styles.css";
+import Image from "next/image";
+import Navigation from "@/app/components/Navigation";
+import BracketTabs from "./components/bracket-tabs";
+import { allBrackets } from "./lib/fifa";
+import MatchesBracket from "./components/matchs-bracket";
+
+export default function Fifa2026() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <main className="relative min-h-screen w-full">
+        <Navigation />
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/background.jpg"
+          alt="FIFA 2026 Background"
+          fill
+          className="object-cover max-h-[1200px]"
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+
+        <div className="absolute inset-0 bg-gradient-to-r from-black via-black/95 to-black/80 z-[0]" />
+
+        {/* <div className="mx-8 relative mt-10">
+          <h1 className="text-4xl font-bold text-white mb-2">
+            Fifa World Cup 26™ Bracket
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <aside className="text-lg">
+            The easiest way to follow along in the tournament. Select a match
+            for more information!
+          </aside>
+        </div> */}
+        <BracketTabs />
+        <div className="relative">
+          <div className="relative flex overflow-hidden pt-10 w-full pb-[30vh]">
+            {allBrackets.map((bracket) => (
+              <MatchesBracket key={bracket.id} bracket={bracket} />
+            ))}
+          </div>
+          <div
+            className="pointer-events-none absolute top-0 left-0 w-full h-full"
+            style={{
+              background:
+                "linear-gradient(to right, transparent 70%, black 100%)",
+            }}
+          />
         </div>
       </main>
-    </div>
+    </>
   );
 }
